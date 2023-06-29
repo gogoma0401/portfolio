aaeac528f5596fc5f8b532123132637121319f89
Jbomip <127369490+Jbomip@users.noreply.github.com>
2023-06-29 17:31:32 +0900

    readme file 내용 추가

---
+++ main/slide.js
@@ -0,0 +1,9 @@
+const won = document.querySelector('.won');
+const items = +getComputedStyle(won).getPropertyValue('--items');
+let index = 0;
+
+setInterval(() => {
+  const step = 360 / items;
+  won.style.setProperty('--rotation', `${(step * index) * -1}deg`)
+  index = index + 1;
+}, 1500);
