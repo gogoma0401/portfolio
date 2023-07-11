c977324a22350657da2c07ca8529829987c41e5e
gogoma0401 <gogoma0401@naver.com>
2023-07-11 16:36:07 +0900

    수정

--- main/slide.js
+++
@@ -1,9 +0,0 @@
-const won = document.querySelector('.won');
-const items = +getComputedStyle(won).getPropertyValue('--items');
-let index = 0;
-
-setInterval(() => {
-  const step = 360 / items;
-  won.style.setProperty('--rotation', `${(step * index) * -1}deg`)
-  index = index + 1;
-}, 1500);
