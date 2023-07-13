c610e16dc7b0abab633bce89c50f5b46bd68619b
gogoma0401 <gogoma0401@naver.com>
2023-07-13 17:17:21 +0900

    완료

--- common/style/slide.js
+++ common/style/slide.js
@@ -6,4 +6,4 @@ setInterval(() => {
   const step = 360 / items;
   won.style.setProperty('--rotation', `${(step * index) * -1}deg`)
   index = index + 1;
-}, 1500);
+}, 1600);
--- index.js
+++ index.js
@@ -13,12 +13,85 @@ function typing() {
     }
 }
 setInterval(typing, 200)
+/**************************************************************************************/
+/**************************************************************************************/
+/**************************************************************************************/
+/**************************************************************************************/
+const imageElements1 = document.querySelectorAll('.image0, .image5, .image10, .image15');
+const projectElement1 = document.querySelector('.project2');
 
-$(function () {
-    $("main .information").click(function () {
-        $(".project1").toggle();
-    });
+imageElements1.forEach((imageElement) => {
+  imageElement.addEventListener('click', () => {
+    projectElement1.style.display = 'block';
+    hideOtherProjects('.project2');
+  });
 });
+
+const imageElements2 = document.querySelectorAll('.image1, .image6, .image11, .image16');
+const projectElement2 = document.querySelector('.project3');
+
+imageElements2.forEach((imageElement) => {
+  imageElement.addEventListener('click', () => {
+    projectElement2.style.display = 'block';
+    hideOtherProjects('.project3');
+  });
+});
+
+const imageElements3 = document.querySelectorAll('.image2, .image7, .image12, .image17');
+const projectElement3 = document.querySelector('.project4');
+
+imageElements3.forEach((imageElement) => {
+  imageElement.addEventListener('click', () => {
+    projectElement3.style.display = 'block';
+    hideOtherProjects('.project4');
+  });
+});
+
+const imageElements4 = document.querySelectorAll('.image3, .image8, .image13, .image18');
+const projectElement4 = document.querySelector('.project5');
+
+imageElements4.forEach((imageElement) => {
+  imageElement.addEventListener('click', () => {
+
+    projectElement4.style.display = 'block';
+    hideOtherProjects('.project5');
+  });
+});
+
+const imageElements5 = document.querySelectorAll('.image4, .image9, .image14, .image19');
+const projectElement5 = document.querySelector('.project1');
+
+imageElements5.forEach((imageElement) => {
+  imageElement.addEventListener('click', () => {
+    projectElement5.style.display = 'block';
+    hideOtherProjects('.project1');
+  });
+});
+
+function hideOtherProjects(currentProjectSelector) {
+  const allProjects = document.querySelectorAll('.project');
+
+  allProjects.forEach((project) => {
+    if (project !== document.querySelector(currentProjectSelector)) {
+      project.style.display = 'none';
+    }
+  });
+}
+
+function hideAllProjects() {
+  const allProjects = document.querySelectorAll('.project');
+
+  allProjects.forEach((project) => {
+    project.style.display = 'none';
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  hideAllProjects();
+});
+
+
+
 
 
 
