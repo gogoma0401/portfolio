55973daee335d63d09eef99407511b109f265a81
gogoma0401 <gogoma0401@naver.com>
2023-07-25 13:17:51 +0900

    수정

--- suwonfc/index.js
+++ suwonfc/index.js
@@ -37,71 +37,60 @@ var swiper = new Swiper(".mySwiper", {
     speed: 1000, // 슬라이드 전환 속도를 1초로 설정 (부드러운 전환을 위해 값을 조정)
 });
 
-/////////////////////////////////////
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-let prevScrollY = 0; // 이전 스크롤 위치를 저장하는 변수
-let isScrolling = false;
-let animationStarted = false; // 애니메이션 시작 여부를 저장하는 변수
-let animationFinished = false; // 애니메이션 종료 여부를 저장하는 변수
-
-function handleScroll() {
-    if (!animationStarted) {
-        animationStarted = true;
-        scrollAnimation();
-    }
-    isScrolling = true;
-}
-
-// 초기 호출
-handleScroll();
-
-// 스크롤 이벤트 리스너 추가
-window.addEventListener('scroll', handleScroll);
-
-function scrollAnimation() {
-    if (isScrolling) {
-        const contentElements = document.querySelectorAll('.big .content span');
-        const scrollPosition = window.scrollY;
-
-        contentElements.forEach((element, index) => {
+////////////////////////////////////
+ function isInViewport(element) {
             const rect = element.getBoundingClientRect();
-            const elementTop = rect.top + scrollPosition;
-            const elementBottom = elementTop + rect.height;
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
+        }
 
-            // 스크롤 위치가 요소의 상단과 하단 사이에 있는지 확인
-            if (scrollPosition >= elementTop && scrollPosition <= elementBottom) {
-                const scrollPercentage = (scrollPosition - elementTop) / (elementBottom - elementTop);
-                // 움직일 거리 설정 (예시로 -40px을 기준으로 움직임)
-                const moveDistance = -40 + (scrollPercentage * 300); // 애니메이션 최대 이동 거리를 300px로 설정
+        let prevScrollY = 0;
+        let isScrolling = false;
+        let animationStarted = false;
+        let animationFinished = false;
 
-                // 요소에 움직임 적용
-                element.style.transform = `translateX(${moveDistance}px)`;
-                element.style.opacity = '1';
-            } else {
-                // 스크롤 역방향일 때, 원래 위치로 돌아가게 처리
-                if (scrollPosition < prevScrollY && scrollPosition >= elementTop) {
-                    element.style.transform = 'translateX(-10px)';
-                    element.style.opacity = '0';
-                }
+        const contentElements = document.querySelectorAll('.big .content span.animation');
+
+        function handleScroll() {
+            if (!animationStarted) {
+                animationStarted = true;
+                scrollAnimation();
             }
-        });
+            isScrolling = true;
+        }
 
-        // 이전 스크롤 위치 갱신
-        prevScrollY = scrollPosition;
-        isScrolling = false;
-    }
+        function scrollAnimation() {
+            if (isScrolling) {
+                const scrollPosition = window.scrollY;
 
-    // 애니메이션 종료 여부 확인 후 다음 리페인트 이전에 다시 함수 호출
-    if (!animationFinished) {
-        requestAnimationFrame(scrollAnimation);
-    }
-}
+                contentElements.forEach(element => {
+                    const animationDuration = parseFloat(window.getComputedStyle(element).animationDuration) * 1000;
+                    const elementOffsetTop = element.offsetTop;
+                    const scrollDistance = Math.abs(scrollPosition - elementOffsetTop);
+
+                    if (scrollDistance <= window.innerHeight / 2) {
+                        if (scrollPosition > prevScrollY) {
+                            element.classList.add('active');
+                        } else {
+                            element.classList.remove('active');
+                        }
+                    } else {
+                        element.classList.remove('active');
+                    }
+
+                    setTimeout(() => {
+                        element.style.opacity = '';
+                    }, animationDuration);
+                });
+
+                prevScrollY = scrollPosition;
+                isScrolling = false;
+                requestAnimationFrame(scrollAnimation);
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
